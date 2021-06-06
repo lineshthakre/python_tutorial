3a925295fe3029d561093f3e747710a7c727542e
Linesh <lthakre18@gmail.com>
2021-06-06 19:59:13 +0530

    data type

--- Python_Basics/tuple.py
+++ Python_Basics/tuple.py
@@ -1,10 +1,39 @@
-my_tuple = {2,4,6,7,67,8,9,5}
+#Tuple Data Type/Data Structure
+
+#Tuple is same as list, list is mutable tuple is immutable.
+#immutable = unchangeable
+#tuples allows duplicate values
+#tuples allows heterogenious daa types 
+
+mytyuple = ("one", "two", 1, True)
+print(mytyuple)
+
+mytuple1 = ("one", "two","three","one")
+print (mytuple1)
+
+
+#Diff of tuples from list
+#syntax
+my_list = [1,2,3,4]
+my_tuple = (1,2,3,4)
+
+print (my_list)
 print (my_tuple)
-#my_tuple.append(7)
-#del del_tupe[2]
-#my_tuple.sort()
-#print (my_tuple[:-1])
-#print (my_tuple[1])
-#print (sorted(my_tuple))
-#print (max(my_tuple))
-print (min(my_tuple))
+
+#mutability
+my_list[2] = 10
+print (my_list)  # 1,2,10,4
+#my_tuple[1] = 10
+print(my_tuple)
+
+
+print(my_list.__sizeof__())
+print(my_tuple.__sizeof__())
+
+
+#List and tuples are same, the only diff is list is mutable and tuple is immutable
+# both are ordered, slicing, duplicate, heterogenious
+
+my_tuple1 = (10,)
+print (my_tuple1)
+print (type(my_tuple1))
