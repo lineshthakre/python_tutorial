ea57c0e5abdf35d42a82ea1b29570ea4b8ac1070
Linesh <lthakre18@gmail.com>
2021-06-06 19:36:39 +0530

    data type

---
+++ Python_Basics/tuple.py
@@ -0,0 +1,10 @@
+my_tuple = {2,4,6,7,67,8,9,5}
+print (my_tuple)
+#my_tuple.append(7)
+#del del_tupe[2]
+#my_tuple.sort()
+#print (my_tuple[:-1])
+#print (my_tuple[1])
+#print (sorted(my_tuple))
+#print (max(my_tuple))
+print (min(my_tuple))
