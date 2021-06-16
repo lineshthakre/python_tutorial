ef47cfd9a9e5a0e772e1b65f12b97cff923dc481
Linesh <lthakre18@gmail.com>
2021-06-16 08:49:51 +0530

    practice

---
+++ Python_Basics/forloop_practice.py
@@ -0,0 +1,18 @@
+weekdays = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+for x in weekdays:
+    print(x)
+
+
+for y in range (100):
+    print(y)
+
+for v in weekdays:
+    if v == 'Mon':
+        continue
+    print(v)
+
+for z in weekdays:
+    if z == 'Thu':
+        break
+    print(z)
+
